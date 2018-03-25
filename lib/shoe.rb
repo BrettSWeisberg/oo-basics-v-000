d2c69bfb392ddbed64f578b05a7b58b055260f91
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-25 17:03:36 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,3 +5,17 @@ class Shoe
   end
 
 end
+
+class Shoe
+  attr_accessor :author, :page_count, :genre
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
